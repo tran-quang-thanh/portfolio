e3db7fac869530cd3637ceb6c89fd53511a86eb8
Tran Quang Thanh <thanh@Trans-MacBook-Pro.local>
2024-06-11 00:28:12 +0800

    Complete nav bar with animation

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,7 +1,9 @@
 'use client'
 import Link from "next/link";
-import { useState } from "react";
-import { Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { ReactNode, useState } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { CgFileDocument } from "react-icons/cg";
 
 export default function NavigationBar() {
   const [isExpanded, updateExpanded] = useState(false);
@@ -24,7 +26,7 @@ export default function NavigationBar() {
 			fixed="top"
 			expand="md"
 			expanded={isExpanded}
-			className={`${navColour ? "sticky" : "navbar"} my-50`}
+			className={`${navColour ? "sticky" : "navbar"}`}
 		>
 			<Navbar.Toggle
 				className="navbar-toggler"
@@ -39,14 +41,56 @@ export default function NavigationBar() {
 			</Navbar.Toggle>
 
 			<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav.Link
-					as={Link}
-					href="/about"
+
+				<NavBarItem 
+					link="/" 
 					onClick={() => updateExpanded(false)}
 				>
-					About
-				</Nav.Link>
+					<AiOutlineHome /> 
+					<div>Home</div>
+				</NavBarItem>
+
+				<NavBarItem 
+					link="/about" 
+					onClick={() => updateExpanded(false)}
+				>
+					<AiOutlineUser />
+					<div>About</div>
+				</NavBarItem>
+
+				<NavBarItem 
+					link="/project" 
+					onClick={() => updateExpanded(false)}
+				>
+					<AiOutlineFundProjectionScreen />
+					<div>Project</div>
+				</NavBarItem>
+
+				<NavBarItem 
+					link="/resume" 
+					onClick={() => updateExpanded(false)}
+				>
+					<CgFileDocument />
+					<div>Resume</div>
+				</NavBarItem>
+
 			</Navbar.Collapse>
 		</Navbar>
   );
+}
+
+function NavBarItem({ link, onClick, children } : {link: string, onClick: () => void, children: ReactNode }) {
+	return (
+		<Nav.Link
+			as={Link}
+			href={link}
+			onClick={onClick}
+			className="group hover:text-[#cd5ff8] hover:scale-110"
+		>
+			<div className="flex items-center space-x-1">
+				{children}
+			</div>
+			<span className="block max-w-0 group-hover:max-w-full duration-500 transition-all h-1.5 bg-[#cd5ff8] rounded"></span>
+		</Nav.Link>
+	);
 }
